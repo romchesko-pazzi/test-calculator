14f8e5c4b954fca8fea394dfc5a1e51444e93ce0
Roma Konstantyuk <romchesko.pazzi@gmail.com>
2023-03-26 19:11:52 +0300

    Delete src/components/keyboard directory

--- src/components/keyboard/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import { KeyboardBox } from '@/assets/commonStyles/keyboard';
-import { renderButton } from '@/components/Keyboard/renderButton';
-
-export const Keyboard = () => {
-  return <KeyboardBox data-cy="keyboard">{renderButton()}</KeyboardBox>;
-};
