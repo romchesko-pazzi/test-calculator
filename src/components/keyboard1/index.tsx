6bd024820487a7a8a656457efec2432a9f5022a0
romchesko-pazzi <romchesko.pazzi@gmail.com>
2023-03-26 19:03:25 +0300

    refactor: review changes

--- src/components/keyboard1/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import { KeyboardBox } from '@/assets/commonStyles/keyboard';
-import { renderButton } from '@/components/keyboard1/renderButton';
-
-export const Keyboard = () => {
-  return <KeyboardBox data-cy="keyboard">{renderButton()}</KeyboardBox>;
-};
